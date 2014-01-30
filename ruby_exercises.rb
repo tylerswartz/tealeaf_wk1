46685d2726ff307eaf6636ddb071449bb596570d
Tyler Swartz <tgswartz@gmail.com>
2014-01-29 21:05:34 -0800

    first ruby practice excercises

---
+++ ruby_exercises.rb
@@ -0,0 +1,48 @@
+#numbers = [1,2,3,4,5,6,7,8,9,10]
+
+#numbers.each do |numb|
+  #puts numb
+  #end
+
+# numbers = [1,2,3,4,5,6,7,8,9]
+# numbers.each {|x| print x}
+
+
+# numbers = [1,2,3,4,5,6,7,8,9,10]
+
+#numbers.each do |numb|
+  #if numb > 5
+    #puts numb
+    #else
+    #end
+  #end
+
+
+#numbers = [1,2,3,4,5,6,7,8,9,10]
+
+#numbers.each do |numb|
+#  if numb > 5
+    #puts numb
+    #else
+    #end
+  #end
+
+
+
+#arr = [1, 2, 3, 4, 5, 6]
+#arr.select { |a| a > 3 }     #=> [4, 5, 6]
+#arr.reject { |a| a < 3 }     #=> [3, 4, 5, 6]
+#arr.drop_while { |a| a < 4 } #=> [4, 5, 6]
+#arr                          #=> [1, 2, 3, 4, 5, 6]
+
+
+#numbers = [1,2,3,4,5,6,7,8,9,10]
+#oddnumb = numbers.select{|n| n.odd?}
+#puts oddnumb
+
+#number = [0,1,2,3,4,5,6,7,8,9,10,11,3]
+#number.uniq!
+#puts number
+
+#h = {a:1, b:2, c:3, d:4}
+hash["a",1,"b",2,"c",3,"d",4]
